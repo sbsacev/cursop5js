02db80efbbde2ce08df9bcd587ddbcf221ae1d03
sbsacev <53238548+sbsacev@users.noreply.github.com>
2023-08-08 12:22:35 -0400

    Delete cap0 directory

--- cap0/sketch.js
+++
@@ -1,9 +0,0 @@
-function setup() {
-  //esta parte se ejecuta una sola vez
-  createCanvas(400,400);
-}
-
-function draw() {
-  //esta parte se ejecuta una y otra vez por toda la eternidad
-  background(220);
-}
