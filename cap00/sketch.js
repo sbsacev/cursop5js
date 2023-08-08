446d6039bad7a28dc5395a3dcc12e61a9243f4d6
sbsacev <53238548+sbsacev@users.noreply.github.com>
2023-08-08 12:25:04 -0400

    Create sketch.js

---
+++ cap00/sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  //esta parte se ejecuta una sola vez
+  createCanvas(400,400);
+}
+
+function draw() {
+  //esta parte se ejecuta una y otra vez por toda la eternidad
+  background(220);
+}
