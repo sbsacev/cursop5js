898ee43151beca40a77fbaa385d33dba41702d33
sbsacev <53238548+sbsacev@users.noreply.github.com>
2023-08-09 14:48:59 -0400

    Create sketch.js - fft mp3

---
+++ cap02/05_1/sketch.js
@@ -0,0 +1,33 @@
+let song;
+let boton;
+let fft;
+
+function preload(){
+  song=loadSound("audio.mp3");  
+}
+
+function setup() {
+  createCanvas(512, 512);
+  boton = createButton('play/pause');
+  boton.mousePressed(toggle);
+  fft = new p5.FFT(0.9,512);
+  colorMode(HSB);
+}
+
+function draw() {
+  background(0);
+  let espectro = fft.analyze();
+  for(let i = 0; i < espectro.length; i++){
+  strokeWeight(4);
+  stroke(map(espectro[i],0,300,0,255),100,100);
+  point(i,map(espectro[i],0,300,height,0));
+  }
+}
+
+function toggle(){
+  if(song.isPlaying()){
+     song.pause();
+     }else{
+    song.play();
+  } 
+}
