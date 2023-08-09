05b6915578a514fc9e173b3eb6ca0edf69a3d99f
sbsacev <53238548+sbsacev@users.noreply.github.com>
2023-08-09 15:00:07 -0400

    Create sketch.js

---
+++ cap03/01_3/sketch.js
@@ -0,0 +1,23 @@
+let pelotas=[];
+
+function setup() {
+  createCanvas(400, 400);
+  for(let i=0; i<10; i++){
+    pelotas[i]= new Pelota(random(100,200),random(100,200));
+  }
+}
+
+function draw() {
+  background(220,100,60,80);
+  for(let i=0; i<pelotas.length; i++){
+    pelotas[i].mostrar();
+    pelotas[i].mover();
+  }
+}
+
+/* // las líneas 12, 13, 14 y 15 puede reemplazarlas por:
+for(let pelotitas of pelotas){
+    pelotitas.mostrar();
+    pelotitas.mover();
+  }
+*/
